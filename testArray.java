05436f2c6f3ebb5f25dc02c621d11c3db6027e50
FranJMC <fran_10_cam@hotmail.com>
2016-05-17 18:01:41 +0200

    Primer test

---
+++ testArray.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testArray {
+
+ArrayAsociativo a;
+
+@Before
+public void setUp(){
+	a = new ArrayAsociativo();
+}
+@Test
+public void addValueWithPut(){
+	ArrayAsociativo b = new ArrayAsociativo();
+	assertArrayEquals(expecteds, actuals);()
+	
+}
+
+}
