b5570dc64a5fff34ae369036597f403c9ed247cb
FranJMC <fran_10_cam@hotmail.com>
2016-05-19 18:54:40 +0200

    completo

---
+++ ArrayAsociativo.java
@@ -0,0 +1,86 @@
+public class ArrayAsociativo {
+	nodo n;
+
+	public class nodo {
+		public String clave;
+		public String valor;
+		public nodo sig;
+
+		public nodo() {
+			clave = null;
+			valor = null;
+			sig = null;
+		}
+	}
+
+	public ArrayAsociativo() {
+		n = new nodo();
+		n.clave = null;
+		n.valor = null;
+	}
+
+	public ArrayAsociativo(String c, String v) {
+		n = new nodo();
+		n.clave = c;
+		n.valor = v;
+	}
+
+	public void put(String c, String v) {
+		if (n.clave == null && n.valor == null) { // vacio
+			n.clave = c;
+			n.valor = v;
+			n.sig = null;
+		} else { // ya tiene elementos
+			n.sig.clave = c;
+			n.sig.valor = v;
+			n.sig.sig = null;
+		}
+	}
+
+	public String get(String clav) {
+		nodo aux = new nodo();
+		aux = n;
+		while ((aux.clave != clav) || (aux.clave != null)) {
+			aux = aux.sig;
+		}
+		return aux.valor;
+	}
+
+	public String getOrElse(String clave, String valorPorDefecto) {
+		String res = valorPorDefecto;
+		if (this.get(clave) != null) {
+			res = this.get(clave);
+		}
+		return res;
+	}
+
+	public int size() {
+		nodo aux = n;
+		int cont = 0;
+		while (aux.clave != null) {
+			cont++;
+		}
+		return cont;
+	}
+
+	public boolean remove(String clave) {
+		boolean res = false;
+		nodo ant = n;
+		nodo curr = n.sig;
+		if (ant.clave == clave) {
+			ant.sig = null;
+			res = true;
+		} else {
+			while ((curr.clave != clave) && (curr.clave != null)) {
+				ant = ant.sig;
+				curr = curr.sig;
+			}
+			if (curr != null) {
+				curr = curr.sig;
+				ant.sig = curr;
+				res = true;
+			}
+		}
+		return res;
+	}
+}
--- testArray.java
+++ testArray.java
@@ -5,17 +5,54 @@ import org.junit.Test;
 
 public class testArray {
 
-ArrayAsociativo a;
+	ArrayAsociativo a;
+	ArrayAsociativo b;
 
-@Before
-public void setUp(){
-	a = new ArrayAsociativo();
-}
-@Test
-public void addValueWithPut(){
-	ArrayAsociativo b = new ArrayAsociativo("nombre","luis");
-	assertArrayEquals(b,a.put("nombre","luis"));
-	
-}
+	@Before
+	public void setUp() {
+		a = new ArrayAsociativo();
+		b = new ArrayAsociativo("nombre", "luis");
+		a.put("nombre", "luis");
+	}
 
+	@Test
+	public void addValueWithPut() {
+		assertEquals(b, a);
+	}
+
+	@Test
+	public void getValue() {
+		assertEquals("luis", a.get("nombre"));
+	}
+
+	@Test
+	public void getOrElseEncontrado() {
+		assertEquals("luis", a.getOrElse("nombre", "hola"));
+	}
+
+	@Test
+	public void getOrElseDefault() {
+		assertEquals("hola", a.getOrElse("nombre", "hola"));
+	}
+
+	@Test
+	public void CalcularTamVacio() {
+		ArrayAsociativo c = new ArrayAsociativo();
+		assertEquals(0, c.size());
+	}
+
+	@Test
+	public void CalcularTamConElem() {
+		assertEquals(1, b.size());
+	}
+
+	@Test
+	public void removeVacio() {
+		assertEquals(a, a.remove("NOMBRE"));
+	}
+
+	@Test
+	public void removeElem() {
+		assertEquals(a, b.remove("nombre"));
+	}
 }
